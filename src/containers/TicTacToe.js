ca496e9ed410c701bab5a0735c81b3c81fd51371
Kamil <kamilcebula1@gmail.com>
2018-10-25 18:06:36 +0200

    added some logic

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react'
 import {Stage} from 'react-konva'
-import {Board} from '../styled/TicTacToe'
+import {Board, Squares} from '../styled/TicTacToe'
 
 class TicTacToe extends Component {
-
+  constructor(props) {
+    super(props)
+    this.combos = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+  }
   state = {
     rows: 3,
     gameState: new Array(9).fill(false),
@@ -15,29 +27,63 @@ class TicTacToe extends Component {
     win: false
   }
 
-  componentWillmount() {
-    //when my component mounts
-    let height = window.innerHeight // we are looking for person browser height
-    console.log(height);
-    let width = window.innerWidth // we are looking for person browser width
-    console.log(width);
+  componentWillMount() {
+    let height = window.innerHeight
+    let width = window.innerWidth
     let size = (height < width) ? height * .8 : width * .8
     let rows = this.state.rows
     let unit = size / rows
+    let coordinates = []
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < rows; x++) {
+        coordinates.push([x*unit, y*unit])
+      }
+    }
 
     this.setState({
       size,
       rows,
       unit,
+      coordinates
     })
   }
 
-  move = () => {
+  move = (marker, index) => {
+    this.setState( (prevState, props) => {
+        let {gameState, yourTurn, gameOver, winner} = prevState
+        yourTurn = !yourTurn
+        
+        return {
 
+        }
+    })
   }
 
-  makeAiMove = () => {
+  makeAiMove = (gameState) => {
+    let otherMark = this.state.otherMark
+    let openSquares = []
+    gameState.forEach( (square, index) => {
+      if(!square) {
+        openSquares.push(index)
+      }
+    })
+    let aiMove = openSquares[this.random(0,openSquares.length)]
+    this.move(aiMove, otherMark)
+    //placeholder
+  }
 
+  random = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max/min)) + min
+  }
+
+  winChecker = (gameState) => {
+    let combos = this.combos
+    return combos.find( (combo) => {
+      let [a,b,c] = combo
+      return (gameState[a] === gameState[b] && gameState[a] === gameState[c] && gameState[a]  )
+    })
   }
 
   turingTest = () => {
@@ -52,9 +98,14 @@ class TicTacToe extends Component {
     let {
       size,
       unit,
-      rows
+      rows,
+      coordinates,
+      gameState,
+      win,
+      gameOver,
+      yourTurn,
+      ownMark
     } = this.state
-
     return (
       <div>
         <Stage
@@ -66,7 +117,16 @@ class TicTacToe extends Component {
             rows={rows}
             size={size}
           />
-          {/* <Squers/> */}
+          <Squares
+            unit={unit}
+            coordinates={coordinates}
+            gameState={gameState}
+            win={win}
+            gameOver={gameOver}
+            yourTurn={yourTurn}
+            ownMark={ownMark}
+            move={this.move}
+          />
         </Stage>
       </div>
     )
--- src/styled/TicTacToe.js
+++ src/styled/TicTacToe.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import {Layer, Line} from 'react-konva'
+import {Layer, Line, Text} from 'react-konva'
 
 export const Board = ({unit, size, rows}) => {
 
   let grid = []
   let stroke = 'grey'
-  let strokeWidth = 10
+  let strokeWidth = 5
   for (let i = 1; i < rows; i++) {
     let position = unit * i
     grid.push(
       <Line
-        points={[0, 0, 100, 0, 100, 100]}
+        points={[position, 0, position, size]}
         stroke={stroke}
         strokeWidth={strokeWidth}
         key={i+'v'}
@@ -18,7 +18,7 @@ export const Board = ({unit, size, rows}) => {
     )
     grid.push(
       <Line
-        points={[0, 0, 100, 0, 100, 100]}
+        points={[0, position, size, position]}
         stroke={stroke}
         strokeWidth={strokeWidth}
         key={i+'h'}
@@ -31,4 +31,51 @@ export const Board = ({unit, size, rows}) => {
       {grid}
     </Layer>
   )
+}
+
+export const Squares = ({
+	unit,
+	coordinates,
+	gameState,
+	win,
+	gameOver,
+	yourTurn,
+	ownMark,
+	move
+}) => {
+	let squares = coordinates.map( (position, index) => {
+			let makeMove = move
+			let mark = gameState[index]
+			let fill = 'black'
+			if (win && win.includes(index)) {
+				fill = 'lightgreen'
+			}
+			if (gameOver || !yourTurn || mark) {
+				makeMove = () => console.log('nope!')
+			}
+			return (
+				<Text
+					key={index}
+					index={index}
+					x={position[0]}
+					y={position[1]}
+					fontSize={unit}
+					width={unit}
+					text={mark}
+					fill={fill}
+					fontFamily={'Helvetica'}
+					align={'center'}
+					onClick={(event)=>{
+						let index = event.target.index
+						makeMove(index, ownMark)
+					}}
+				/>
+			)
+	})
+
+	return (
+		<Layer>
+			{squares}
+		</Layer>
+	)
 }
